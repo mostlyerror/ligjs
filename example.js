d8bcca7308aaef70d3e62b0f5f8d769f5288c218
Benjamin Poon <benjamintpoon@gmail.com>
2019-02-04 17:55:52 -0700

    initial commit

---
+++ example.js
@@ -0,0 +1,11 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://champion.gg/champion/orianna/middle')
+  await page.screenshot({path: 'example.png'})
+
+  await browser.close()
+})()
+
